461621c904d209c077ff1b9d2420968ddbd9b243
Kortney Stinson <kortneystinson@gmail.com>
2020-09-15 11:57:20 -0400

    Fix smooth scroll offset on main page

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,7 +1,7 @@
   $("a").click(function(e){
     var $anchor = $(this);
     $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top
+        scrollTop: $($anchor.attr('href')).offset().top - 250
     }, 1500);
     e.preventDefault(); //this is the important line.
 });
